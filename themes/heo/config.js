2f46bfb353bcd4538822ef414df84a20e97133d5
Chunyu Hu <hunao1995@gmail.com>
2025-03-15 20:48:40 -0400

    更新博客配置，将主题修改为'movie'，并调整建站日期和通知条内容

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,12 +4,12 @@ const CONFIG = {
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2023-04-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '你好，我们是已经在美国登记结婚的Hugh&Edward', url: 'https://www.hughandedward.com' },
+    { title: 'YOLO: You only live once.', url: '' }
   ],
 
   // 英雄区左右侧组件颠倒位置
